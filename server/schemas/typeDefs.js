b3ffec3858135f7570aa3c3da5c99ee4a62a664d
Eric <ekellogg90@gmail.com>
2024-08-27 16:03:12 -0500

    added typedefs and index

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const typeDefs = /* GraphQL */`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type InputBook {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, passowrd: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(newBook: InputBook!): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
